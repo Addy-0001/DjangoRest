63b94eef70188b0090d5c6e77f55fe5d3724fcda
Adamya Neupane <adamsneupane@gmail.com>
2023-11-29 12:25:00 +0545

    1 hour 5 minutes into the course

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,28 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
+from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET', 'POST'])
 def api_home(request, *args, **kwargs):
-    # request.body
-    body = request.body  # we're assuming that this is going to be a byte string of json data
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
+    # if model_data:
+    #     data['id'] = model_data.id
+    #     data['title'] = model_data.title
+    #     data['content'] = model_data.content
+    #     data['price'] = model_data.price
+    #     #This whole thing is called serialization.
+    #     # Model Instance = model_data.
+    #     # turn it into a python dictionary
+    #     # return json to the client
 
-    try:
-        data = json.loads(body) # Takes a string of JSON data and turns it into a python dictionary
-    except:
-        pass
-    print(body)
-
-    print(data)
-
-    return JsonResponse({"message": "Hi there, this is your django api response!"})
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return JsonResponse(data)
